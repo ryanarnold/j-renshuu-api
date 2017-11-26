dcdd28b6d690eb21e4e870079f059167bc78abcc
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-27 06:39:47 +0800

    Update test script.

--- j_renshuu_api/tests.py
+++ j_renshuu_api/tests.py
@@ -4,5 +4,7 @@ from requests import get, post, put, delete
 SERVER = 'http://localhost:8000/'
 
 response = get(SERVER + 'categories/')
+print(response.json())
 
+response = get(SERVER + 'words/')
 print(response.json())
